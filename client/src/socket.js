162ec507a687b957b44f87169c5a5f6856c99206
deepaksharma1810 <deepaks7081@gmail.com>
2026-06-10 10:08:28 +0530

    initial commit

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://lms-library-management-system-9nhw.onrender.com", {
   transports: ["websocket"], // faster & stable
 });
 
