deaf620350d1706d51df3189d0f7b432eca796e8
deepaksharma1810 <deepaks7081@gmail.com>
2026-06-10 10:49:42 +0530

    initial commit

--- server/utils/mail.js
+++ server/utils/mail.js
@@ -10,10 +10,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-
-  // connectionTimeout: 10000,
-  // greetingTimeout: 10000,
-  // socketTimeout: 10000,
 });
 
 // VERIFY SMTP CONNECTION
