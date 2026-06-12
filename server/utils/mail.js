7c0cef0686884214901b501d099e5b609139c854
deepaksharma1810 <deepaks7081@gmail.com>
2026-06-12 17:38:12 +0530

    changed port

--- server/utils/mail.js
+++ server/utils/mail.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // CREATE TRANSPORTER (SMTP BASED)
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || "smtp.gmail.com",
-  port: process.env.SMTP_PORT || 587,
+  port: process.env.SMTP_PORT || 465,
   secure: true, // TLS after connect
   auth: {
     user: process.env.SMTP_USER,
