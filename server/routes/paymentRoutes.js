260f6f20588535340e8c1ebd100e6631c0ed5057
deepaksharma1810 <deepaks7081@gmail.com>
2026-07-17 16:07:34 +0530

    SET CHECKOUT

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const router = express.Router();
 
 const {
   createPaymentOrder,
   verifyPayment,
 } = require("../controllers/paymentController");
 const verifyToken = require("../middleware/verifyJWT");
-
-const router = express.Router();
 
 router.route("/create-order").post(verifyToken, createPaymentOrder);
 router.route("/verify").post(verifyToken, verifyPayment);
